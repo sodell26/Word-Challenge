f472934c0dcc9007540311346cdb7cfbd32ca1da
Sarah O'Dell <sarah.mo26@gmail.com>
2021-08-22 10:09:38 -0600

    removed in-sentence punctuation

--- server.js
+++ server.js
@@ -7,12 +7,17 @@ wordCounter = (paragraph) => {
 	for(i=0; i<paragraph.length; i++) { //create array of sentences, each element will be a single  sentence
 		if(i == paragraph.length-1 || (paragraph[i] == "." && paragraph[i+1] == " " && paragraph[i+2] == paragraph[i+2].toUpperCase())){
 			//what will toUpperCase do with non-letters
-			//will need to deal with other sentence-ending punctuation
+			//will need to deal with other sentence-ending punctuation (regex)
 			sentenceArr.push(paragraph.substring(sentenceStart, i))
 			sentenceStart = i+2;
-			console.log(sentenceArr)
-			
+			// console.log(sentenceArr
+
 		}
+	}
+
+	for(i=0; i<sentenceArr.length; i++) { //removes in-sentence punctuation, so I'm left with words separated only by spaces
+		let currentString = sentenceArr[i].replace(/[:,]/g,'')
+		console.log(currentString)
 	}
 }
 
