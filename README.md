8945b20ee0e569bfea6c9fd5c62d5646db473ec9
Sarah O'Dell <sarah.mo26@gmail.com>
2021-08-26 19:57:50 -0600

    added to README; cleaned up code and changed special characters check to not include .

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Word-Challenge
+# Word Untangl-ing   
+
+This is my solution for solving a coding challenge to count and list the amount of occurances a word is used in a given paragraph. This is written primarily with Javascript, but with some HTML and CSS for usability. No libraries nor depencies were used.    
+
+Total time spent was roughly 7 hours. I spent around 4.5 hours solving the challenge with a Javascript function. Then 2.5 hours of testing, bug fixing,	 and setup.   
+
+My code is done with a simple Javascript/CSS/HTML format. To ensure this runs correclty, please be sure all files are in the same folder, then open the html in your browser by dragging and dropping the html file into an open browser window.    
+
+The expected result of this code is an alphabetical list where each line looks as follows:   
+- numbering.   word  {occurences: sentence number of occurence, sentence number of next occurence, etc}   
+
+The list will be alphabetical by the first letter of each word.   
+
+To use the app, simply add your paragraph(s) to the text box. Then, click the button labeled "organize" to see the list of words underneath the text box.   
+Sentences must start and end gramatically correct with sentence-ending punctuation and starting with a capital letter.   
+You can edit or add more to your paragraph if you wish, or replace it all either by selecting your original input and typing new paragraphs, or clear the whole selection with the "clear" button.   
+
+The "clear" button clears both the text box and the list. 
+
+The "organize" button will organize whatever is in the text box.   
+
+
+
--- server.js
+++ server.js
@@ -1,42 +1,33 @@
-//test paragraph
-// let paragraph = "Given an arbitrary text document written in English, write a program that will generate a concordance, i.e. an alphabetical list of all word occurrences, labeled with word frequencies. Bonus: label each word with the sentence numbers in which each occurrence appeared.";
-
-
-
 wordCounter = (orgParagraph) => {
-	let paragraph = orgParagraph.replace(/[\r\n]+/g," ");	
+	let paragraph = orgParagraph.replace(/[\r\n]+/g," "); //remove line breaks first
 	let result = [];
 	let sentenceArr = [];
 	let sentenceStart = 0;
 	for(i=0; i<paragraph.length; i++) { //create array of sentences, each element will be a single  sentence
-		if(i == paragraph.length-1 || ((paragraph[i] == "." || paragraph[i] == "?" || paragraph[i] == "!") && paragraph[i+1] == " " && paragraph[i+2] == paragraph[i+2].toUpperCase())){
-			//what will toUpperCase do with non-letters
-			//will need to deal with other sentence-ending punctuation (regex)
-			sentenceArr.push(paragraph.substring(sentenceStart, i))
-			sentenceStart = i+2;
-			// console.log(sentenceArr)
-
+		if(i == paragraph.length-1 || ((paragraph[i] == "." || paragraph[i] == "?" || paragraph[i] == "!") && paragraph[i+1] == " " && paragraph[i+2] == paragraph[i+2].toUpperCase())){ //checks if i is either the string end or theres a combo of a sentence-ending punctuation, space, and capital letter
+			sentenceArr.push(paragraph.substring(sentenceStart, i)) //if combo exists, put string from the start, to character before i (ending punctuation) as it's own element in sentenceArr
+			sentenceStart = i+2; //sentence start will now be the capital letter from the combo
 		}
 	}
 
 	for(i=0; i<sentenceArr.length; i++) { //removes in-sentence punctuation, so I'm left with words separated only by spaces
-		let currentString = sentenceArr[i].replace(/[^\w\s]/gi,'');
-		// console.log(currentString)
+		let currentString = sentenceArr[i].replace(/[,:;@#$%&*()"']/gi,''); //regex to check for special characters, I didn't want to use the regex for all special characters as it removes the "." from "i.e."
+
 		let arrOfWords = currentString.split(' '); //separate the word in string into an array, using space as delimiter
-		// console.log(arrOfWords) 
+
 		for(j=0; j<arrOfWords.length; j++) { //loop through arrOfWords
-			let foundInArr = false; //check words first so it doesn't keep adding to the array
-			for(k=0; k<result.length; k++) { //check if word already exists in result
-				if(result[k].word == arrOfWords[j]){ //uptick the counter
+			let foundInArr = false; //setting up boolean for checking if words first, so it doesn't keep adding to the array
+			for(k=0; k<result.length; k++) { //loop to check if word already exists in result
+				if(result[k].word == arrOfWords[j]){ //if word exists, add occurence, but don't add word to array
 					foundInArr = true;
 					break;
 				}
 			}
 
-			if(foundInArr) {
-				result[k].occurences.push(i+1);				
+			if(foundInArr) { //word exists condition
+				result[k].occurences.push(i+1);	//using i+1 since i could be 0			
 
-			} else { //create new object in result array, if word doesn't exist in the result
+			} else { //if word doesn't exist in the result, create new object in result array
 				result.push(
 					{
 						word: arrOfWords[j],
@@ -46,7 +37,8 @@ wordCounter = (orgParagraph) => {
 			}
 		}
 	}
-	result.sort((compare1, compare2) => { //alphabetize the words, has objects so have to tell sort() how to sort
+
+	result.sort((compare1, compare2) => { //alphabetize the words, result has objects so have to tell sort() how to sort
 		if (compare1.word == compare2.word) {
 			return 0;
 		} else if (compare1.word.toLowerCase() < compare2.word.toLowerCase()){
@@ -55,41 +47,34 @@ wordCounter = (orgParagraph) => {
 			return 1;
 		}
 	})
-	for (i=0; i<result.length; i++) { //final result
 
-		// console.log(result[i].word + " " + result[i].occurences.length + ":" + result[i].occurences.join(", "))
+	for (i=0; i<result.length; i++) { //final result that's sent to HTML
 
-		let olList = document.getElementById("listOfWords");
-		addListItem = document.createElement("li");
+		let olList = document.getElementById("listOfWords"); //reference ol on HTML
+		addListItem = document.createElement("li"); //create the li that will have the result info added to it
 
-		newListItem = document.createTextNode(result[i].word.toLowerCase() + " {" + result[i].occurences.length + ":" + result[i].occurences.join(", ") + "}");
-		addListItem.appendChild(newListItem);
-		olList.appendChild(addListItem);
+		newListItem = document.createTextNode(result[i].word.toLowerCase() + " {" + result[i].occurences.length + ":" + result[i].occurences.join(", ") + "}"); //creae the text for the li
+		addListItem.appendChild(newListItem); //add the text to the li
+		olList.appendChild(addListItem);// add the li to the ol
 	}
 
-	// console.log(result);
 
 }
 
-// wordCounter(paragraph);
 
 
-submitBtn = () => {
+submitBtn = () => { //function to run wordCounter() when user clicks "Organize!" button
 	document.getElementById("listOfWords").innerHTML = "";
 	let userParagraph = document.getElementById("para").value.trim();
 	document.getElementById("submitBtn").addEventListener("click", wordCounter(userParagraph));
 }
 
-clearBtn = () => {
+clearBtn = () => { //function to clear text box and ol when user clicks "clear" button
 	document.getElementById("listOfWords").innerHTML = "";
 	document.getElementById("para").value = "";
 }
 
 
-
-//to do
-//*add more punctuation
-//remove console comments
 
 
 
